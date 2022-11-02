2bdcde2ef9ad740e5960b31c46c92e190907acab
Tjohn5703 <tjohn5703@gmail.com>
2022-11-02 17:10:43 -0400

    add hashes then salted 10 times

--- app.js
+++ app.js
@@ -12,23 +12,18 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
-const encrypt = require("mongoose-encryption");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/userDB")
+mongoose.connect("mongodb://127.0.0.1:27017/userDB")
 
 const userSchema = new mongoose.Schema({
   email: String,
   password: String
 });
 
-userSchema.plugin(encrypt, {
-  secret: process.env.SECRET,
-  encryptedFields: ["password"]
-});
-
 const User = new mongoose.model("User", userSchema);
-
 
 app.get("/", function(req, res) {
   res.render("home");
@@ -43,16 +38,18 @@ app.get("/register", function(req, res) {
 });
 
 app.post("/register", function(req, res) {
-  const newUser = new User({
-    email: req.body.username,
-    password: req.body.password
-  });
-  newUser.save(function(err) {
-    if (!err) {
-      res.render("secrets");
-    } else {
-      console.log(err);
-    }
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+    const newUser = new User({
+      email: req.body.username,
+      password: hash
+    });
+    newUser.save(function(err) {
+      if (!err) {
+        res.render("secrets");
+      } else {
+        console.log(err);
+      }
+    });
   });
 });
 
@@ -67,9 +64,14 @@ app.post("/login", function(req, res) {
       console.log(err);
     } else {
       if (foundUser) {
-        if (foundUser.password === password) {
-          res.render("secrets");
-        }
+        bcrypt.compare(password, foundUser.password, function(err, result){
+          if(result === true){
+            res.render("secrets");
+          }else{
+            res.render("login");
+          }
+        });
+
       }
     }
   });
